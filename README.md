94f7c6083ae5dccdcd7ba34bc90f46b5bc994736
Owsiej <owsiejaszek@gmail.com>
2024-09-16 20:16:58 +0200

    refactored table component using @rx-angular/state library

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
   - symulacje wykonano poprzez dodanie operatora delay z bilbioteki rxjs, za pomocą operatora tap uruchamiamy spinnera a następnie podczas subskrypcji jest on wyłączany; w przypadku, gdyby dane rzeczywiście były pobierane z jakiegoś zewnętrznego api za pomocą http request, wtedy logika spinnera zostałaby obsłużona przez interceptor
 - dodaj możliwość edycji dowolnej wartości rekordu wyświetlonego w tabeli, po zatwierdzeniu zmian, wiersz tabeli powinien się zaktualizować; edycja powinna odbywać się bez mutowaniia danych - ✔️ wykonano
   - przy każdej wartości mamy ikonkę, po kliknięcia na którą wyskakuje popup z możliwością wpisania nowej wartości, od siebie dorzuciłem małą walidację z wykorzystaniem regexu, tak że dla pól string akceptowane są tylko małe i duże litery a dla pol number integery oraz float (np. 234.423)
-  - dane zmieniają się tylko na poziomie komponentu, nawet w serwisie pozostaje niezmienione
+  - dane zmieniają się tylko w stanie komponentu
 - dodaj filtr, który pozwoli na filtrowanie wyników (jeden input filtrujący po wszystkich polach), filtrowanie powinno odbywać się po 2s bez zmiany wartości w inpucie - ✔️ wykonano
   - opóźnienie filtrowania wykonane za pomocą operatora debounceTime z biblioteki rxjs
 - jako bibliotekę do komponentów użyj https://material.angular.io/ - ✔️ wykonano
--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { PeriodicElement } from '../../models/periodic-elements';
 import { DataService } from '../../services/data.service';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import {
-  BehaviorSubject,
-  combineLatest,
-  debounceTime,
-  delay,
-  distinctUntilChanged,
-  tap,
-} from 'rxjs';
+import { BehaviorSubject, debounceTime, delay, map, tap } from 'rxjs';
 import { LoadingService } from '../../services/loading.service';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialog } from '@angular/material/dialog';
@@ -18,6 +11,13 @@ import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
+import { RxState } from '@rx-angular/state';
+
+interface ElementDataState {
+  data: MatTableDataSource<PeriodicElement>;
+  filteredValue: string;
+  updateValue: string;
+}
 
 @Component({
   selector: 'app-table',
@@ -32,9 +32,10 @@ import { MatInputModule } from '@angular/material/input';
   ],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss',
+  providers: [RxState],
 })
-export class TableComponent implements OnInit {
-  public data = new MatTableDataSource<PeriodicElement>();
+export class TableComponent {
+  public dataSource = new MatTableDataSource<PeriodicElement>();
   public tableColumns: (keyof PeriodicElement)[] = [
     'position',
     'name',
@@ -44,29 +45,37 @@ export class TableComponent implements OnInit {
 
   public filterData$ = new BehaviorSubject<string>('');
 
+  public elementsDataAsMatTableDataSource$ = this._state.select(
+    ['data', 'filteredValue'],
+    (state) => {
+      state.data.filter = state.filteredValue.trim().toLowerCase();
+      return state.data;
+    }
+  );
   constructor(
     private _dataService: DataService,
     private _loadingService: LoadingService,
-    private _dialogService: MatDialog
-  ) {}
-
-  ngOnInit(): void {
-    combineLatest([
+    private _dialogService: MatDialog,
+    private _state: RxState<ElementDataState>
+  ) {
+    this._state.connect(
+      'data',
       this._dataService.elementsData$.pipe(
         tap(() => this._loadingService.loadingOn()),
-        delay(5000)
-      ),
-      this.filterData$.pipe(
-        tap(() => this._loadingService.loadingOn()),
-        debounceTime(2000),
-        distinctUntilChanged()
-      ),
-    ]).subscribe(([sourceData, filterData]) => {
-      console.log('test');
-      this._loadingService.loadingOff();
-      this.data.data = sourceData;
-      this.data.filter = filterData.trim().toLowerCase();
-    });
+        delay(5000),
+        map((elementsData) => {
+          const dataSource = this.dataSource;
+          dataSource.data = elementsData;
+          return dataSource;
+        }),
+        tap(() => this._loadingService.loadingOff())
+      )
+    );
+
+    this._state.connect(
+      'filteredValue',
+      this.filterData$.pipe(debounceTime(2000))
+    );
   }
 
   openDialog(key: keyof PeriodicElement, element: PeriodicElement): void {
@@ -75,14 +84,21 @@ export class TableComponent implements OnInit {
     });
 
     dialogRef.afterClosed().subscribe((result) => {
-      console.log(result);
       if (result !== undefined) {
-        const index = this.data.data.findIndex((e) => e === element);
+        const index = this._state
+          .get('data')
+          .data.findIndex((e) => e === element);
         if (index !== -1) {
           if (key === 'symbol' || key === 'name') {
-            this.data.data[index][key] = result as string;
+            this._state.set('data', (state) => {
+              state.data.data[index][key] = result as string;
+              return state.data;
+            });
           } else if (key === 'position' || key === 'weight') {
-            this.data.data[index][key] = result as number;
+            this._state.set('data', (state) => {
+              state.data.data[index][key] = result as number;
+              return state.data;
+            });
           }
         }
       }
